ee2bc2979bec6d3d24a7c5cf890db332f55967ec
raobarbalho <84943528+raobarbalho@users.noreply.github.com>
2021-06-16 09:28:13 -0300

    5
    5

---
+++ EXERCICIOS-JS/FUNDAMENTOS/Booleanos.js
@@ -0,0 +1,14 @@
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log (!!isAtivo)
+
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
+console.log()
