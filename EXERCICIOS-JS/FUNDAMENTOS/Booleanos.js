581ca0b31fc141deba8063dd43d2444ca0ef07b0
raobarbalho <84943528+raobarbalho@users.noreply.github.com>
2021-06-16 16:56:19 -0300

    Update Booleanos.js

--- EXERCICIOS-JS/FUNDAMENTOS/Booleanos.js
+++ EXERCICIOS-JS/FUNDAMENTOS/Booleanos.js
@@ -6,9 +6,25 @@ console.log (!!isAtivo)
 
 console.log('os verdadeiros...')
 console.log(!!3)
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
-console.log()
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('para finalizar...')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = 'Raoni'
+
+console.log(nome || 'Desconhecido')
