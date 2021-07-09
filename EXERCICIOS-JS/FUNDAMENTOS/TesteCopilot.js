375376f5a85dfc571ff040ec201ce2bffba8240b
raobarbalho <84943528+raobarbalho@users.noreply.github.com>
2021-07-08 21:36:31 -0300

    11

---
+++ EXERCICIOS-JS/FUNDAMENTOS/TesteCopilot.js
@@ -0,0 +1,14 @@
+// create a function to test the copilot
+function TesteCopilot() {
+    var copilot = new Copilot();
+    var resultado = copilot.calcula(5, 5);
+    alert("Resultado: " + resultado);
+    console.log("Resultado: " + resultado);
+}
+// create a function to find subarray sum equals to zero
+function SubarraySumEqualsZero() {
+    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var resultado = findSubarraySumEqualsZero(array);
+    alert("Resultado: " + resultado);
+    console.log("Resultado: " + resultado);
+}
